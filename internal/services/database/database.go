963d4643d6a9b22594aba495e540d8bc21c76b33
Michael Schwieger <git@z4vr.dev>
2022-05-20 10:52:03 +0200

    postgres stuff in the works

--- internal/services/database/database.go
+++ internal/services/database/database.go
@@ -1,9 +1,26 @@
 package database
 
+import "errors"
+
+var ErrValueNotFound = errors.New("value not found in database")
+
 // Database is the interface for a database driver.
 type Database interface {
 
-	// GENERAL
+	// GeneralL
 	Connect(credentials ...interface{}) error
 	Close()
+
+	// Guilds
+	GetGuildBotMessageChannelID(guildID string) (channelID string, err error)
+	SetGuildBotMessageChannelID(guildID, channelID string) error
+
+	GetGUildLevelUpMessage(guildID string) (message string, err error)
+	SetGuildLevelUpMessage(guildID, message string) error
+
+	GetGuildAFKChannelID(guildID string) (channelID string, err error)
+	SetGuildAFKChannelID(guildID, channelID string) error
+
+	GetGuildAutoRolesID(guildID string) (roleID []string, err error)
+	SetGuildAutoRolesID(guildID, roleID []string) error
 }
--- internal/services/database/postgres/postgres.go
+++ internal/services/database/postgres/postgres.go
@@ -2,8 +2,8 @@ package postgres
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
+	"strings"
 
 	_ "github.com/lib/pq"
 	"github.com/z4vr/subayai/internal/models"
@@ -14,10 +14,7 @@ type PGMiddleware struct {
 	Db *sql.DB
 }
 
-var (
-	_                database.Database = (*PGMiddleware)(nil)
-	ErrValueNotFound                   = errors.New("value not found")
-)
+var _ database.Database = (*PGMiddleware)(nil)
 
 func (p *PGMiddleware) setup() (err error) {
 	if err = p.Db.Ping(); err != nil {
@@ -35,8 +32,8 @@ func (p *PGMiddleware) setup() (err error) {
 		"guild_id" varchar (25) NOT NULL,
 		"bot_message_channel_id" varchar (25) NOT NULL DEFAULT '',
 		"level_up_message" text DEFAULT 'Well done {user}, your Level of wasting time just advanced to {level}!',
-		"afk_channel_id" text NOT NULL DEFAULT '',
-		"autorole_ids" text NOT NULL DEFAULT '',
+		"afk_channel_id" varchar (25) NOT NULL DEFAULT '',
+		"autorole_ids" varchar (25) NOT NULL DEFAULT '',
 		PRIMARY KEY ("guild_id"));
 	`)
 	if err != nil {
@@ -64,6 +61,37 @@ func (p *PGMiddleware) setup() (err error) {
 		"totalxp" integer NOT NULL,
 		PRIMARY KEY ("entry_id"));
 	`)
+	if err != nil {
+		return
+	}
+
+	// create timestamp table
+	_, err = tx.Exec(`
+	CREATE TABLE IF NOT EXISTS "timestamp" (
+		"entry_id" serial NOT NULL,
+		"user_id" varchar (25) NOT NULL,
+		"guild_id" varchar (25) NOT NULL,
+		"last_message_ts" timestamp NOT NULL,
+		"last_session_ts" timestamp NOT NULL,
+		PRIMARY KEY ("entry_id"));
+	`)
+	if err != nil {
+		return
+	}
+
+	// create discordids table
+	_, err = tx.Exec(`
+	CREATE TABLE IF NOT EXISTS "discordids" (
+		"entry_id" serial NOT NULL,
+		"user_id" varchar (25) NOT NULL,
+		"guild_id" varchar (25) NOT NULL,
+		"last_message_id" varchar (25) NOT NULL,
+		"last_session_id" varchar (25) NOT NULL,
+		PRIMARY KEY ("entry_id"));
+	`)
+	if err != nil {
+		return
+	}
 
 	return
 }
@@ -85,4 +113,132 @@ func (p *PGMiddleware) Close() {
 	if p.Db != nil {
 		p.Db.Close()
 	}
+}
+
+func (p *PGMiddleware) getGuildSetting(guildID, setting string) (value string, err error) {
+
+	err = p.Db.QueryRow(`
+	SELECT `+setting+` FROM guild WHERE guild_id = $1;
+	`, guildID).Scan(&value)
+
+	err = wrapNotFound(err)
+
+	return
+}
+
+func (p *PGMiddleware) setGuildSetting(guildID, setting, value string) (err error) {
+
+	res, err := p.Db.Exec(`
+	UPDATE guild SET `+setting+` = $1 WHERE guild_id = $2;
+	`, value, guildID)
+	if err != nil {
+		return
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return
+	}
+
+	if rows == 0 {
+		_, err = p.Db.Exec(`
+		INSERT INTO guild (guild_id, `+setting+`) VALUES ($1, $2);
+		`, guildID, value)
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func wrapNotFound(err error) error {
+	if err == sql.ErrNoRows {
+		err = database.ErrValueNotFound
+	}
+	return err
+}
+
+// GetGuildBotMessageChannelID returns the bot message channel ID for the guild
+func (p *PGMiddleware) GetGuildBotMessageChannelID(guildID string) (value string, err error) {
+	return p.getGuildSetting(guildID, "bot_message_channel_id")
+}
+
+// SetGuildBotMessageChannelID sets the bot message channel ID for the guild
+func (p *PGMiddleware) SetGuildBotMessageChannelID(guildID, value string) (err error) {
+	return p.setGuildSetting(guildID, "bot_message_channel_id", value)
+}
+
+// GetGuildLevelUpMessage returns the level up message for the guild
+func (p *PGMiddleware) GetGuildLevelUpMessage(guildID string) (value string, err error) {
+	return p.getGuildSetting(guildID, "level_up_message")
+}
+
+// SetGuildLevelUpMessage sets the level up message for the guild
+func (p *PGMiddleware) SetGuildLevelUpMessage(guildID, value string) (err error) {
+	return p.setGuildSetting(guildID, "level_up_message", value)
+}
+
+// GetGuildAfkChannelID returns the afk channel ID for the guild
+func (p *PGMiddleware) GetGuildAfkChannelID(guildID string) (value string, err error) {
+	return p.getGuildSetting(guildID, "afk_channel_id")
+}
+
+// SetGuildAfkChannelID sets the afk channel ID for the guild
+func (p *PGMiddleware) SetGuildAfkChannelID(guildID, value string) (err error) {
+	return p.setGuildSetting(guildID, "afk_channel_id", value)
+}
+
+// GetGuildAutoroleIDs returns the autorole IDs for the guild
+func (p *PGMiddleware) GetGuildAutoroleIDs(guildID string) (value []string, err error) {
+
+	roleString, err := p.getGuildSetting(guildID, "autorole_ids")
+	if err != nil {
+		return []string{}, err
+	} else if roleString == "" {
+		return []string{}, nil
+	}
+
+	return strings.Split(roleString, ";"), nil
+}
+
+// SetGuildAutoroleIDs sets the autorole IDs for the guild
+func (p *PGMiddleware) SetGuildAutoroleIDs(guildID string, value []string) (err error) {
+	roleString := strings.Join(value, ";")
+	return p.setGuildSetting(guildID, "autorole_ids", roleString)
+}
+
+func (p *PGMiddleware) getUserSetting(userID, setting string) (value string, err error) {
+
+	err = p.Db.QueryRow(`
+	SELECT `+setting+` FROM user WHERE user_id = $1;
+	`, userID).Scan(&value)
+
+	err = wrapNotFound(err)
+
+	return
+}
+
+func (p *PGMiddleware) setUserSetting(userID, setting, value string) (err error) {
+
+	res, err := p.Db.Exec(`
+	UPDATE user SET `+setting+` = $1 WHERE user_id = $2;
+	`, value, userID)
+	if err != nil {
+		return
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return
+	}
+
+	if rows == 0 {
+		_, err = p.Db.Exec(`
+		INSERT INTO user (user_id, `+setting+`) VALUES ($1, $2);
+		`, userID, value)
+		if err != nil {
+			return
+		}
+	}
+	return
 }
